e9663c614d5496e133d39f4b577dc24af49fd828
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-02-12 09:00:40 +0530

    added a employee table and tabel description

--- postgres-learning/create-table/employee_scahma.sql
+++ postgres-learning/create-table/employee_scahma.sql
@@ -1,16 +1,15 @@
-CREATE TABLE management.employees (
-    emp_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    dept_id BIGINT NOT NULL,
-    full_name VARCHAR(150) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL, -- Index created automatically!
-    salary NUMERIC(12, 2) CHECK (salary > 0),
-    is_active BOOLEAN DEFAULT TRUE,
-    
-    CONSTRAINT fk_department 
-        FOREIGN KEY(dept_id) 
-        REFERENCES management.departments(dept_id) 
-        ON DELETE RESTRICT
+create table management.employees(
+id bigint generated always as identity primary key,
+full_name varchar(100) not null,
+email varchar(60) not null,
+phone_number varchar(20) not null,
+hire_date date not null default current_date,
+job_id bigint not null references management.jobs(id),
+depart_id bigint not null references management.departments(id),
+created_at timestamptz  default current_timestamp,
+updated_at timestamptz  default current_timestamp,
+deleted_at timestamptz default null
 );
-
--- Create this one manually for performance on JOINs
-CREATE INDEX idx_employees_dept_id ON management.employees(dept_id);
+create unique index unique_email_index on management.employees(email) where deleted_at is null;
+create index idx_employee_depart_id on management.employees(depart_id);
+create index idx_employee_job_id on management.employees(job_id);
